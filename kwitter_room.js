d3f755173ba503fe42452eee2a357d0aa443c84c
Coding-Kiten <71798143+Coding-Kiten@users.noreply.github.com>
2021-01-24 21:35:34 -0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,12 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyADFb4D-i_Zh5bjUCS-CCLTrTI6vA0JJhY",
-    authDomain: "kwitter-7fd45.firebaseapp.com",
-    databaseURL: "https://kwitter-7fd45-default-rtdb.firebaseio.com",
-    projectId: "kwitter-7fd45",
-    storageBucket: "kwitter-7fd45.appspot.com",
-    messagingSenderId: "557556687594",
-    appId: "1:557556687594:web:53405453afc38ad266ed93"
+    apiKey: "AIzaSyAQ5KABdcthJ-6YjQChZtzyM6PMFSUSbDw",
+    authDomain: "dfghjk-e6054.firebaseapp.com",
+    databaseURL: "https://dfghjk-e6054-default-rtdb.firebaseio.com",
+    projectId: "dfghjk-e6054",
+    storageBucket: "dfghjk-e6054.appspot.com",
+    messagingSenderId: "55773051324",
+    appId: "1:55773051324:web:65da326a669ffc5ada13b9",
+    measurementId: "G-VGQT2H1NG6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
